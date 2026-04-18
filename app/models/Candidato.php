479c63ff15c31275591fbe2c9763b0776ed722d2
rafaelgomesdc <rafaelgomesdc0000@gmail.com>
2026-04-17 23:58:19 -0300

    feat: implementando models (CRUD)

--- app/models/Candidato.php
+++ app/models/Candidato.php
@@ -9,9 +9,26 @@ class Cadidato
     {
         $this->conn = $db;
     }
-    
+
     public function Registrar($dados)
-    {}
+    {
+        if ($this->Find($cpf) === nul);
+        {
+            //Registrar
+        }
+        else{
+            echo . "CPF já cadastrado.";
+        }
+    }
+
+    public Find($cpf)
+    {
+        $sql = "SELECT * FROM {$this->table} WHERE cpf = :cpf";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute([':cpf' = $cpf]);
+
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
 
     public function Carregar($dados)
     {}
--- app/models/Vaga.php
+++ app/models/Vaga.php
@@ -9,6 +9,52 @@ class Vaga
     {
         $this->conn = $db;
     }
+
+    public function Store($dados) //Gravar dados
+    {
+        $sql = "INSERT INTO {$this->table} () VALUES (:)";
+        $stmt = $this->conn->prepare($sql);
+
+        return $stmt->execute([
+            ':' => $dados['']
+        ]);
+    }
+
+    public function Update($dados)
+    {
+        $sql = "UPDATE {$this->table}
+                SET col = :col
+                WHERE col_id = :id";
+        $stmt = $this->conn->prepare($sql);
+
+        return $stmt->execute([
+            ':col' => $dados['col'],
+            ':col_id' => $dados['id']
+        ]);
+    }
+
+    public function Delete($cod)
+    {
+        $sql = "DELETE FROM {$this->} WHERE cod = :cod";
+        $stmt = $this->conn->prepare($sql);
+
+        return $stmt->execute([':cod' => $cod]);
+    }
+
+    public function All()
+    {
+        $stmt = $this->conn->query("SELECT * FROM {$this->table}");
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function Find($cod)
+    {
+        $sql = "SELECT * FROM {$this->table} WHERE cod = :cod";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute([':cod' => $cod]);
+
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
 }
 
 ?>
