c5216dcdf3fdb55b2a297de578a6518f3e516260
DevGustavo-alt <depolitobentogustavo@gmail.com>
2026-04-23 09:07:29 -0300

    feat:criação cada página login

---
+++ laravel/resources/views/login/login.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Faça o login para se manter por dentro das atualizações!</title>
+</head>
+<body>
+<header>
+        <div class="header-content">
+            <div class="identidade">
+                <img src="../../../public/imagens/logo.png">
+                <h1>PrudenVagas</h1>
+            </div>
+        </div>
+    </header>
+    <section>
+        <h1>Fazer login como:</h1>
+        <a href="login-pessoa-fisica.html">Candidato</a>
+        <a href="login-pessoa-juridica.html">Empresa</a>
+        <a href="login-gerenciador.html">Gerenciador</a>
+    </section>
+</body>
+</html>
